41201814d6181269456be8c83c8a94517b42f1bb
Tadas Goberis <61449927+tadasgo@users.noreply.github.com>
2020-12-13 13:56:16 +0200

    Removed local data and integrated items from DB

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -3,11 +3,21 @@ import memoize from 'lodash.memoize';
 
 const selectShop = (state) => state.shop;
 
-export const selectCollections = createSelector([selectShop], (shop) => shop.collections);
+export const selectCollections = createSelector(
+	[selectShop],
+	(shop) => shop.collections
+);
 
 // transfor object to array
-export const selectCollectionsForPrieview = createSelector([selectCollections], (collections) => Object.values(collections));
+export const selectCollectionsForPrieview = createSelector(
+	[selectCollections],
+	(collections) => (collections ? Object.values(collections) : [])
+);
 
 // Memoize does the same idea of memoization as reselect does for our selectors, except this time we're memoizing the return of our function
 // selector is memoized by reselect
-export const selectCollection = memoize((collectionUrlParam) => createSelector([selectCollections], (collections) => collections[collectionUrlParam]));
+export const selectCollection = memoize((collectionUrlParam) =>
+	createSelector([selectCollections], (collections) =>
+		collections ? collections[collectionUrlParam] : null
+	)
+);
